dcba289e4cea293e3ecf5ab03d033aef554f58ef
Olatunde Adekola <tunde@aeternum.co>
2025-08-12 16:44:00 +0100

    Added modified template for portfolio page

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # projects-portfolio
+
+To host the page locally,
+
+Navigate to `projects-portfolio`
+
+`cd projects-portfolio`
+
+Then run `npm install` to install dependencies 
+
+Run `npm run dev` to go to `http://localhost:4321/` on browser
+
+
+Modified a template from [RyanFitzgerald](https://github.com/RyanFitzgerald/devportfolio)
---
+++ src/config.ts
@@ -0,0 +1,39 @@
+export const siteConfig = {
+  name: "Tunde",
+  title: "A Data Engineer (Microsoft Certified)",
+  description: "Portfolio website of Olatunde Adekola",
+  accentColor: "#9c38e9fa",
+  social: {
+    email: "adekolaolat@gmail.com",
+    linkedin: "https://www.linkedin.com/in/olatunde-adekola-7893b6178/",
+    
+    github: "https://github.com/adekolaolat",
+  },
+  aboutMe:
+    "I’ve worked across data roles as both a Data Scientist and a Data Engineer, helping businesses turn messy data into clear and actionable insights. My work is mainly in Azure, where I design and maintain scalable, efficient pipelines using modern data architectures, lakehouses, data warehouses, and real-time streaming systems to power analytics. I make sure Analysts and Data Scientists have clean, reliable, and business-ready  data so they can focus on uncovering insights, not fixing data problems.",
+  skills: ["Big Data","Python", "SQL / T-SQL", "Azure", "Fabric","Databricks","PySpark", "Linux","Git", "SQL Server","Postgres", "PowerBI", "Tableau",],
+  projects: [
+    {
+      name: "Bus Operations in Liverpool",
+      description:
+        "Out of an interest in understanding Liverpool’s bus operations, I developed an end-to-end Azure data pipeline that pulls live data from the Bus Open Data Service (BODS) API. The solution processes and transforms over 3 million data points, making it easy to analyze and report on city’s bus network.",
+      link: "https://github.com/adekolaolat/bods-liverpool-azure-data-engineering",
+      skills: ["PySpark","SQL/T-SQL","ETL","Azure Data Factory", "ADLS", "Databricks", "Synapse Analytics","Medallion lakehouse architecture","Git","PowerBI"],
+    },
+    {
+      name: "On-Prem to Cloud Data Integration with Microsoft Fabric",
+      description:
+        "Developed a Microsoft Fabric data pipeline to lands on-premises data into a lakehouse and transform it for analytics. For a business procurement use case, I created a dashboard that identifies high-cost suppliers and flags regular suppliers with low product costs, helping to monitor product quality and drive cost reduction efforts.",
+      link: "https://github.com/adekolaolat/fabric-data-engineering-on-premises-db",
+      skills: ["Fabric","PySpark", "SQL/T-SQL", "Data Factory","Data Migration","Lakehouse", "Data modelling", "Git","PowerBI"],
+    },
+    {
+      name: "Data Governance: Implementing Granular Access Control",
+      description:
+        "This project implements a security aspect of data governance at a granular level by applying features such as Dynamic Data Masking (DDM), Column-Level Security (CLS), and Row-Level Security (RLS) within a business setting.",
+      link: "https://github.com/adekolaolat/granular-data-security-sql-server-fabric",
+      skills: ["T-SQL", "SQL Server", "SSMS","Fabric", "Azure"],
+    },
+  ],
+  
+};
